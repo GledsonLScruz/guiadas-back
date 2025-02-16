698ed8aae22ba2edb72b76645350774bf2cfec95
unknown <gledsonlscruz@gmail.com>
2025-02-16 18:36:02 -0300

    changes

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,5 +1,6 @@
 import * as express from "express";
 import { createUser, getUsers } from '../services/userService';
+import { UserRepository } from "../repository/userRepository";
 
 const router = express.Router();
 
@@ -11,7 +12,8 @@ router.post('/', async (req, res) => {
             res.status(400).json({ message: "Todos os campos são obrigatórios" });
         }
 
-        const user = await userRepo.createUser(username, email, password);
+        let userRepository = new UserRepository();
+        const user = await  userRepository.createUser(username, email, password,startSemester,enrolledCourseId);
         res.json(user); // Retorna o usuário criado
     } catch (error: any) {
         res.status(500).json({ message: "Erro ao criar o usuário", error: error.message });
@@ -20,7 +22,8 @@ router.post('/', async (req, res) => {
 
 router.get('/', async (req, res) => {
     try {
-        const users = await userRepo.getAllUsers();
+        let userRepository = new UserRepository();
+        const users = await userRepository.getAllUsers();
         res.json(users); // Retorna todos os usuários
     } catch (error: any) {
         res.status(500).json({ message: "Erro ao obter os usuários", error: error.message });
--- src/repository/userRepository.ts
+++ src/repository/userRepository.ts
@@ -3,12 +3,15 @@ import { User } from "../models/user";
 export class UserRepository {
 
     // Criar um novo usuário
-    async createUser(name: string, email: string, password: string) {
+    async createUser(name: string, email: string, password: string, startSemester: string,
+        enrolledCourseId: string,) {
         // Use o método `create` para salvar no banco de dados
         return await User.create({
             username: name,
             email,
-            password
+            password,
+            startSemester,
+            enrolledCourseId
         });
     }
 
